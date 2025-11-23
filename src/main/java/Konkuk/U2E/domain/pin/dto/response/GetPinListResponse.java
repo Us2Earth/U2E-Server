010da1c291b2f8c8703d1d7a99e2d7814bf1de26
janghyunjun <buzz03312@naver.com>
2025-11-23 15:33:09 +0900

    [refactor] 핀 조회시 최신순 70개만 끊어서 보여지도록 수정

--- src/main/java/Konkuk/U2E/domain/pin/dto/response/GetPinListResponse.java
+++ src/main/java/Konkuk/U2E/domain/pin/dto/response/GetPinListResponse.java
@@ -1,11 +1,16 @@
 package Konkuk.U2E.domain.pin.dto.response;
 
+import java.util.Comparator;
 import java.util.List;
 
 public record GetPinListResponse(
         List<PinInfo> pinList
 ) {
     public static GetPinListResponse of(List<PinInfo> pinList) {
-        return new GetPinListResponse(pinList);
+        List<PinInfo> result = pinList.stream()
+                .sorted(Comparator.comparing(PinInfo::pinId).reversed()) // pinId 기준 내림차순
+                .limit(70)                                               // 최대 70개
+                .toList();
+        return new GetPinListResponse(result);
     }
 }
